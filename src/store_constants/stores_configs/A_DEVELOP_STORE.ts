a27f30ad256302741b78bd64989932860e7a876b
Dmytro Orgish <dmytro.orgish88@gmail.com>
2025-09-30 13:20:16 +0300

    wip

--- src/store_constants/stores_configs/A_DEVELOP_STORE.ts
+++ src/store_constants/stores_configs/A_DEVELOP_STORE.ts
@@ -2,8 +2,8 @@ import { ALBERTO_BINI_KZ_WS } from "./ALBERTO_BINI_KZ_WS"; // eslint-disable-lin
 import { ALBERTO_BINI_MAIN } from "./ALBERTO_BINI_MAIN"; // eslint-disable-line
 import { ALBERTO_BINI_SALES } from "./ALBERTO_BINI_SALES"; // eslint-disable-line
 import { KREMEN_BELTS_MAIN } from "./KREMEN_BELTS_MAIN"; // eslint-disable-line
-import { RETAIL_ALBERTO_BINI } from "./RETAIL_ALBERTO_BINI";
-import { STONED_CLUB } from "./STONED_CLUB";
+import { RETAIL_ALBERTO_BINI } from "./RETAIL_ALBERTO_BINI"; // eslint-disable-line
+import { STONED_CLUB } from "./STONED_CLUB"; // eslint-disable-line
 
 export const DEVELOP_STORE = {
   // ...ALBERTO_BINI_MAIN,
